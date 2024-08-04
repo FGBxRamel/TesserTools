dcd4bd9feeb4f6144aaad5abab9d1605eb234a20
Raziel Kretschmer <30479611+FGBxRamel@users.noreply.github.com>
2024-08-04 21:20:25 +0200

    Initial

---
+++ src/main/java/de/bydora/tesserTools/TesserTools.java
@@ -0,0 +1,17 @@
+package de.bydora.tesserTools;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class TesserTools extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
