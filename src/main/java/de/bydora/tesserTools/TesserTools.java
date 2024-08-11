b5dbdc05e13bec5f96cf21b6e8778e496a8acd4b
Raziel Kretschmer <30479611+FGBxRamel@users.noreply.github.com>
2024-08-11 17:46:33 +0200

    feat: Drop fire charges on block break

--- src/main/java/de/bydora/tesserTools/TesserTools.java
+++ src/main/java/de/bydora/tesserTools/TesserTools.java
@@ -1,8 +1,9 @@
 package de.bydora.tesserTools;
 
-import de.bydora.tesserTools.commands.CommandTesbug;
+import de.bydora.tesserTools.listeners.BlockDropItemListener;
 import de.bydora.tesserTools.listeners.InvMoveItemListener;
 import de.bydora.tesserTools.listeners.PlayerInteractListener;
+import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class TesserTools extends JavaPlugin {
@@ -11,13 +12,19 @@ public final class TesserTools extends JavaPlugin {
     @Override
     public void onEnable() {
         this.getLogger().info("TesserTools is enabled");
-        this.getCommand("tesbug").setExecutor(new CommandTesbug());
-        getServer().getPluginManager().registerEvents(new InvMoveItemListener(), this);
-        getServer().getPluginManager().registerEvents(new PlayerInteractListener(), this);
+        // this.getCommand("tesbug").setExecutor(new CommandTesbug());
+        this.registerListeners();
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+    }
+
+    private void registerListeners() {
+        final PluginManager pm = getServer().getPluginManager();
+        pm.registerEvents(new InvMoveItemListener(), this);
+        pm.registerEvents(new PlayerInteractListener(), this);
+        pm.registerEvents(new BlockDropItemListener(), this);
     }
 }
