29f5f7c2607c0a03f32663f5b7a30a5a300c40ff
Raziel Kretschmer <30479611+FGBxRamel@users.noreply.github.com>
2024-12-17 22:24:58 +0100

    feat(enchantment): Add functionality to "LifeSteal"

--- src/main/java/de/bydora/tesserTools/enchantment/enchantments/Lifesteal.java
+++ src/main/java/de/bydora/tesserTools/enchantment/enchantments/Lifesteal.java
@@ -1,14 +1,20 @@
 package de.bydora.tesserTools.enchantment.enchantments;
 
+import de.bydora.tesserTools.TesserTools;
 import de.bydora.tesserTools.enchantment.enums.EnchantmentSpaceKeys;
 import de.bydora.tesserTools.enchantment.util.EquipmentGroups;
 import org.bukkit.Material;
 import org.bukkit.NamespacedKey;
+import org.bukkit.attribute.Attribute;
+import org.bukkit.attribute.AttributeInstance;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.persistence.PersistentDataContainer;
 import org.bukkit.persistence.PersistentDataType;
+import org.bukkit.scheduler.BukkitScheduler;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.Arrays;
@@ -22,8 +28,23 @@ public class Lifesteal implements CustomEnchantment<EntityDamageByEntityEvent> {
     private final static Material[] enchantableItems = EquipmentGroups.SWORDS;
 
     @Override
+    @EventHandler(ignoreCancelled = true)
     public void enchantmentEvent(EntityDamageByEntityEvent event) {
-
+        if (event.getDamager() instanceof Player player
+            && event.isCritical()
+        ) {
+            AttributeInstance absorption = player.getAttribute(Attribute.MAX_ABSORPTION);
+            if (absorption.getBaseValue() < 20) {
+                absorption.setBaseValue(20);
+            }
+            final double newAbsorption = player.getAbsorptionAmount() + 2;
+            player.setAbsorptionAmount(newAbsorption);
+            BukkitScheduler scheduler = player.getServer().getScheduler();
+            scheduler.runTaskLater(TesserTools.getPlugin(TesserTools.class), () -> {
+                final double newAmount = player.getAbsorptionAmount() - 2;
+                player.setAbsorptionAmount(newAmount > 0 ? newAmount : 0);
+                    }, 200);
+        }
     }
 
     @Override
