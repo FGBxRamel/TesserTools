c0d34456f7c424b7b37ffce8ee4bc153b0aafa64
Raziel Kretschmer <30479611+FGBxRamel@users.noreply.github.com>
2025-01-14 23:22:19 +0100

    feat(enchantment): Enchanting

--- src/main/java/de/bydora/tesserTools/enchantment/blocks/ExtEnchantingTable.java
+++ src/main/java/de/bydora/tesserTools/enchantment/blocks/ExtEnchantingTable.java
@@ -3,49 +3,69 @@ package de.bydora.tesserTools.enchantment.blocks;
 import com.destroystokyo.paper.ParticleBuilder;
 import de.bydora.tesserTools.TesserTools;
 import de.bydora.tesserTools.enchantment.enchantments.CustomEnchantment;
+import de.bydora.tesserTools.enchantment.enchantments.Missing;
 import de.bydora.tesserTools.enchantment.enums.EnchantmentSpaceKeys;
 import de.bydora.tesserTools.enchantment.exceptions.NotAnEnchantmentTableException;
 import io.papermc.paper.registry.RegistryAccess;
 import io.papermc.paper.registry.RegistryKey;
+import io.papermc.paper.registry.TypedKey;
+import net.kyori.adventure.key.Key;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
+import net.kyori.adventure.text.serializer.plain.PlainTextComponentSerializer;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.Particle;
+import org.bukkit.Registry;
 import org.bukkit.block.EnchantingTable;
 import org.bukkit.enchantments.Enchantment;
+import org.bukkit.entity.Display;
+import org.bukkit.entity.TextDisplay;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.persistence.PersistentDataContainer;
 import org.bukkit.persistence.PersistentDataType;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
+import java.util.*;
+import java.util.logging.Logger;
 
+import static java.util.Map.entry;
+
+@SuppressWarnings("rawtypes")
 public class ExtEnchantingTable {
+
+    Logger log = TesserTools.getPlugin(TesserTools.class).getLogger();
+
+    private static final Map ENCHANTMENT_MAP = TesserTools.getPlugin(TesserTools.class).getEnchantmentMap();
+    public static final Registry<Enchantment> ENCHANTMENT_REGISTRY = RegistryAccess.registryAccess().getRegistry(RegistryKey.ENCHANTMENT);
+    private static final Map<Integer, String> ROMAN_NUMERALS = Map.ofEntries(
+            entry(1, "I"),
+            entry(2, "II"),
+            entry(3, "III"),
+            entry(4, "IV"),
+            entry(5, "V"),
+            entry(6, "VI")
+    );
+    private final static Collection<CustomEnchantment> CUSTOM_ENCHANTMENTS = TesserTools.getPlugin(TesserTools.class)
+            .getEnchantmentMap().values();
 
     private final Location[] simpleQuarzLocations;
     private final Location[] simpleLapisLocations;
     private final Location location;
     private final List<Enchantment> vanillaEnchantments;
-    private final static Collection<CustomEnchantment> customEnchantments = TesserTools.getPlugin(TesserTools.class)
-            .getEnchantmentMap().values();
     private boolean isBlocked;
     private int chargeLevel;
-    private final PersistentDataContainer stateContainer;
+    private final List<Object> rolledEnchantments = new ArrayList<>();
+    private final EnchantingTable vanillaTable;
 
     public ExtEnchantingTable(@NotNull Location location) {
 
         this.location = location;
 
         if (location.getBlock().getState() instanceof EnchantingTable table) {
-            this.stateContainer = table.getPersistentDataContainer().getOrDefault(
-                    EnchantmentSpaceKeys.STATE_CONTAINER.getKey(), PersistentDataType.TAG_CONTAINER,
-                    table.getPersistentDataContainer().getAdapterContext().newPersistentDataContainer()
-            );
-            this.isBlocked = this.stateContainer.getOrDefault(EnchantmentSpaceKeys.STATE_BLOCKED.getKey(),
-                    PersistentDataType.BOOLEAN, false);
-            this.chargeLevel = this.stateContainer.getOrDefault(EnchantmentSpaceKeys.STATE_CHARGE_LEVEL.getKey(),
-                    PersistentDataType.INTEGER, 0);
+            this.vanillaTable = table;
+            loadState();
         }
         else {
             throw new NotAnEnchantmentTableException("You can only make an instance of ExtEnchantingTable from" +
@@ -54,7 +74,7 @@ public class ExtEnchantingTable {
 
         final Location calcQuarzLocation = location.clone().toCenterLocation();
         this.simpleQuarzLocations = new Location[]{
-                calcQuarzLocation.add(4, 1, 4).clone(),
+                calcQuarzLocation.add(4, 0, 4).clone(),
                 calcQuarzLocation.add(-8, 0, 0).clone(),
                 calcQuarzLocation.add(0, 0, -8).clone(),
                 calcQuarzLocation.add(8, 0, 0).clone()
@@ -62,14 +82,14 @@ public class ExtEnchantingTable {
         // Reset so the calc is right
         final Location calcLapisLocation = location.clone().toCenterLocation();
         this.simpleLapisLocations = new Location[]{
-                calcLapisLocation.add(6, 1, 0).clone(),
+                calcLapisLocation.add(6, 0, 0).clone(),
                 calcLapisLocation.add(-12, 0, 0).clone(),
                 calcLapisLocation.add(6, 0, -6).clone(),
                 calcLapisLocation.add(0, 0, 12).clone()
         };
 
         this.vanillaEnchantments = new ArrayList<>();
-        for (Enchantment enchantment : RegistryAccess.registryAccess().getRegistry(RegistryKey.ENCHANTMENT)) {
+        for (Enchantment enchantment : ENCHANTMENT_REGISTRY) {
             vanillaEnchantments.add(enchantment);
         }
     }
@@ -97,6 +117,14 @@ public class ExtEnchantingTable {
             if (materialBottom != Material.LAPIS_BLOCK || materialTop != Material.LAPIS_BLOCK) { return false; }
         }
         return true;
+    }
+
+    /**
+     * Determines if the table is valid for enchantments
+     * @return Whether it is valid for enchantments
+     */
+    public boolean isValid() {
+        return (this.hasValidLapis() && this.hasValidQuarz());
     }
 
     public void spawnParticles() {
@@ -127,8 +155,89 @@ public class ExtEnchantingTable {
     }
 
     public void setBlocked(boolean blocked) {
-        isBlocked = blocked;
-        this.stateContainer.set(EnchantmentSpaceKeys.STATE_BLOCKED.getKey(), PersistentDataType.BOOLEAN, blocked);
+        this.isBlocked = blocked;
+        this.saveState();
+    }
+
+    public Location getLocation() {
+        return this.location;
+    }
+
+    /**
+     * Get the {@link CustomEnchantment} or {@link Enchantment} of the quarz block given.
+     * @param loc The location of the quarz block
+     * @return The {@link CustomEnchantment} or {@link Enchantment} or null
+     */
+    public @Nullable Object getEnchantment(@NotNull Location loc) {
+        int i = 0;
+        for (var location : simpleQuarzLocations) {
+            // Check if it's the same block location... Bad, but works
+            if (Math.abs(loc.getX() - location.getX()) < 1
+                && Math.abs(loc.getY() - location.getY()) < 1
+                && Math.abs(loc.getZ() - location.getZ()) < 1
+            ) {
+                return this.rolledEnchantments.get(i);
+            }
+            i++;
+        }
+        return null;
+    }
+
+    /**
+     * Loads the state of the enchantment table from it's {@link PersistentDataContainer}.
+     */
+    private void loadState() {
+        var stateContainer = this.vanillaTable.getPersistentDataContainer().getOrDefault(
+                EnchantmentSpaceKeys.STATE_CONTAINER.getKey(), PersistentDataType.TAG_CONTAINER,
+                this.vanillaTable.getPersistentDataContainer().getAdapterContext().newPersistentDataContainer()
+        );
+        this.isBlocked = stateContainer.getOrDefault(EnchantmentSpaceKeys.STATE_BLOCKED.getKey(),
+                PersistentDataType.BOOLEAN, false);
+        this.chargeLevel = stateContainer.getOrDefault(EnchantmentSpaceKeys.STATE_CHARGE_LEVEL.getKey(),
+                PersistentDataType.INTEGER, 0);
+
+        //<editor-fold desc="Loading the enchantments">
+        var enchantments = stateContainer.get(EnchantmentSpaceKeys.STATE_ENCHANTMENTS.getKey(),
+                PersistentDataType.LIST.strings());
+        if (enchantments == null) {
+            var missing = new Missing();
+            for (int i = 0; i < 5; i++) {
+                this.rolledEnchantments.add(missing);
+            }
+        }
+        else {
+            for (var ench : enchantments) {
+                Object enchantment = null;
+                if (ench.startsWith("tessertools:")) {
+                    enchantment = ENCHANTMENT_MAP.get(ench);
+                } else if (ench.startsWith("minecraft:")) {
+                    enchantment = ENCHANTMENT_REGISTRY.get(TypedKey.create(RegistryKey.ENCHANTMENT,
+                            Key.key(ench)));
+                }
+                this.rolledEnchantments.add(Objects.requireNonNullElseGet(enchantment, Missing::new));
+            }
+        }
+        //</editor-fold>
+    }
+
+    /**
+     * Saves the states of the enchantment table to it's {@link PersistentDataContainer}.
+     */
+    private void saveState() {
+        var stateContainer = this.vanillaTable.getPersistentDataContainer().getOrDefault(
+                EnchantmentSpaceKeys.STATE_CONTAINER.getKey(), PersistentDataType.TAG_CONTAINER,
+                this.vanillaTable.getPersistentDataContainer().getAdapterContext().newPersistentDataContainer()
+        );
+        stateContainer.set(EnchantmentSpaceKeys.STATE_BLOCKED.getKey(), PersistentDataType.BOOLEAN,
+                this.isBlocked);
+        stateContainer.set(EnchantmentSpaceKeys.STATE_CHARGE_LEVEL.getKey(), PersistentDataType.INTEGER,
+                this.chargeLevel);
+        stateContainer.set(EnchantmentSpaceKeys.STATE_ENCHANTMENTS.getKey(), PersistentDataType.LIST.strings(),
+                this.getEnchantmentStrings());
+        this.vanillaTable.getPersistentDataContainer().set(EnchantmentSpaceKeys.STATE_CONTAINER.getKey(),
+                PersistentDataType.TAG_CONTAINER, stateContainer);
+
+        this.vanillaTable.update();
     }
 
     /**
@@ -136,7 +245,7 @@ public class ExtEnchantingTable {
      * @param item The {@link ItemStack} to check
      * @return A list of vanilla enchantments valid for the given {@link ItemStack}.
      */
-    public List<? extends Enchantment> getVanillaEnchantments(ItemStack item) {
+    private List<? extends Enchantment> getVanillaEnchantments(ItemStack item) {
         var enchantments = new ArrayList<Enchantment>();
 
         for (var enchantment : vanillaEnchantments) {
@@ -152,10 +261,9 @@ public class ExtEnchantingTable {
      * @param item The {@link ItemStack} to check
      * @return A list of custom enchantments valid for the given {@link ItemStack}.
      */
-    @SuppressWarnings("rawtypes")
-    public List<CustomEnchantment> getCustomEnchantments(ItemStack item) {
+    private List<CustomEnchantment> getCustomEnchantments(ItemStack item) {
         var enchantments = new ArrayList<CustomEnchantment>();
-        for (var enchantment : customEnchantments) {
+        for (var enchantment : CUSTOM_ENCHANTMENTS) {
             if (enchantment.canEnchantItem(item)
                 && enchantment.getEnchantmentLevel(item) < enchantment.getMaxLevel()
             ) {
@@ -164,4 +272,101 @@ public class ExtEnchantingTable {
         }
         return enchantments;
     }
+
+    /**
+     * Creates four random enchantments. The statistical distribution is 3:1 (vanilla:custom).
+     * @param item The item that should be enchanted.
+     */
+    public void startEnchanting(ItemStack item) {
+        var customs = getCustomEnchantments(item);
+        Collections.shuffle(customs);
+        List<Object> mixedList = new ArrayList<>(getVanillaEnchantments(item));
+        for (int i = mixedList.size() / 3; i > 0; i--) {
+            mixedList.add(customs.removeFirst());
+        }
+        if (mixedList.size() < 4) { return; }
+        Collections.shuffle(mixedList);
+
+        this.rolledEnchantments.clear();
+        for (int i = 1; i <= 4; i++) {
+            this.rolledEnchantments.add(mixedList.removeFirst());
+        }
+        this.saveState();
+        showEnchantments(item);
+
+    }
+
+    private void showEnchantments(ItemStack itemStack) {
+        removeDisplays();
+
+        int i = 0;
+        for (var enchantment : this.rolledEnchantments) {
+            if (enchantment instanceof CustomEnchantment<?>) {
+                int nextLevel = getNextEnchantmentLevel(itemStack, (CustomEnchantment) enchantment);
+                String roman = nextLevel <= ROMAN_NUMERALS.size() ? ROMAN_NUMERALS.get(nextLevel) : "UNDEFINED";
+                spawnText(this.simpleQuarzLocations[i].clone().add(0,1,0),
+                        ((CustomEnchantment<?>) enchantment).getDisplayName() + " " + roman,
+                        true);
+            }
+            else if (enchantment instanceof Enchantment) {
+                int nextLevel = getNextEnchantmentLevel(itemStack, (Enchantment) enchantment);
+                spawnText(this.simpleQuarzLocations[i].clone().add(0,1,0),
+                        PlainTextComponentSerializer.plainText().serialize(((Enchantment) enchantment).displayName(nextLevel)),
+                        false);
+            }
+            i++;
+        }
+    }
+
+    public void removeDisplays() {
+        for (var display : location.getNearbyEntitiesByType(Display.class, 6,6,6)) {
+            display.remove();
+        }
+    }
+
+    private void spawnText(Location location, String text, boolean red) {
+        // https://www.spigotmc.org/threads/tutorial-holograms-1-8.65183/
+        TextDisplay display = location.getWorld().spawn(location, TextDisplay.class, entity -> {
+            entity.text(Component.text(text, red ? NamedTextColor.DARK_RED : NamedTextColor.WHITE));
+            entity.setBillboard(Display.Billboard.VERTICAL);
+            entity.setPersistent(false);
+        });
+    }
+
+    /**
+     * Gets the next possible level of the vanilla enchantment.
+     * @param itemStack The {@link ItemStack} that should be checked
+     * @param enchantment The {@link Enchantment} to check
+     * @return The next possible level
+     */
+    private int getNextEnchantmentLevel(ItemStack itemStack, Enchantment enchantment) {
+        return itemStack.getEnchantmentLevel(enchantment) + 1;
+    }
+
+    /**
+     * Gets the next possible level of the custom enchantment.
+     * @param itemStack The {@link ItemStack} that should be checked
+     * @param enchantment The {@link CustomEnchantment} to check
+     * @return The next possible level
+     */
+    private int getNextEnchantmentLevel(ItemStack itemStack, CustomEnchantment enchantment) {
+        return enchantment.getEnchantmentLevel(itemStack) + 1;
+    }
+
+    /**
+     * Get a list of the ids of the current rolled enchantments.
+     * @return A list of enchantment ids
+     */
+    private List<String> getEnchantmentStrings() {
+        var enchantments = new ArrayList<String>();
+        for (var enchantment : this.rolledEnchantments) {
+            if (enchantment instanceof Enchantment) {
+                enchantments.add(((Enchantment) enchantment).getKey().toString());
+            } else if (enchantment instanceof CustomEnchantment) {
+                enchantments.add(((CustomEnchantment<?>) enchantment).getID());
+            }
+        }
+        return enchantments;
+    }
+
 }
--- src/main/java/de/bydora/tesserTools/enchantment/listeners/PlayerDropItemListener.java
+++ src/main/java/de/bydora/tesserTools/enchantment/listeners/PlayerDropItemListener.java
@@ -2,12 +2,19 @@ package de.bydora.tesserTools.enchantment.listeners;
 
 import de.bydora.tesserTools.TesserTools;
 import de.bydora.tesserTools.enchantment.blocks.ExtEnchantingTable;
+import de.bydora.tesserTools.enchantment.enchantments.CustomEnchantment;
 import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.Material;
 import org.bukkit.block.EnchantingTable;
+import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Item;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerDropItemEvent;
+import org.bukkit.inventory.ItemStack;
+
+import javax.annotation.Nullable;
 
 public class PlayerDropItemListener implements Listener {
 
@@ -16,25 +23,97 @@ public class PlayerDropItemListener implements Listener {
         Item item = event.getItemDrop();
         final int[] metadata = {0, 0}; // I hate Java; first is the number of tries, second a "bool" if it landed or not
 
-        // Try every 5 Ticks if the item has landed
-        Bukkit.getScheduler().runTaskTimer(TesserTools.getPlugin(TesserTools.class), new Runnable() {
-            @Override
-            public void run() {
-                if (metadata[0] >= 3) {Bukkit.getScheduler().cancelTasks(TesserTools.getPlugin(TesserTools.class));}
-                else if (item.isOnGround()
-                        && item.isValid()
-                        && item.getLocation().getBlock().getState() instanceof EnchantingTable table
-                ) {
-                    ExtEnchantingTable extTable = new ExtEnchantingTable(item.getLocation());
-                    extTable.setBlocked(true);
-                    extTable.spawnParticles();
+        if (item.getItemStack().getType() == Material.LAPIS_LAZULI) {
+            Bukkit.getScheduler().runTaskTimer(TesserTools.getPlugin(TesserTools.class), new Runnable() {
+                @Override
+                public void run() {
+                    if (metadata[0] >= 3) {
+                        Bukkit.getScheduler().cancelTasks(TesserTools.getPlugin(TesserTools.class));
+                    } else if (item.isOnGround()
+                            && item.isValid()
+                            && item.getLocation().clone().add(0,-1,0).getBlock().getType() == Material.CHISELED_QUARTZ_BLOCK
+                    ) {
+                        var quarzLocation = item.getLocation().clone().add(0, -1,0).toCenterLocation();
+                        ExtEnchantingTable extTable = getExtTable(quarzLocation);
+                        if (extTable == null) {
+                            Bukkit.getScheduler().cancelTasks(TesserTools.getPlugin(TesserTools.class));
+                            return;
+                        }
+                        Item enchantItem = extTable.getLocation().getNearbyEntitiesByType(
+                                Item.class, 1,2,1).iterator().next();
+                        ItemStack enchantStack = enchantItem.getItemStack();
+                        var enchantment = extTable.getEnchantment(quarzLocation);
+                        switch (enchantment) {
+                            case null -> {
+                                Bukkit.getScheduler().cancelTasks(TesserTools.getPlugin(TesserTools.class));
+                                return;
+                            }
+                            case CustomEnchantment<?> customEnch -> {
+                                customEnch.enchantItem(enchantStack,
+                                        customEnch.getEnchantmentLevel(enchantStack) + 1);
+                            }
+                            case Enchantment vanillaEnch -> {
+                                enchantStack.addUnsafeEnchantment(vanillaEnch,
+                                        enchantStack.getEnchantmentLevel(vanillaEnch) + 1);
+                            }
+                            default -> {
+                            }
+                        }
+                        item.remove();
+                        extTable.removeDisplays();
 
-                    Bukkit.getScheduler().cancelTasks(TesserTools.getPlugin(TesserTools.class));
+
+                        Bukkit.getScheduler().cancelTasks(TesserTools.getPlugin(TesserTools.class));
+                    } else {
+                        metadata[0]++;
+                    }
                 }
-                else {
-                    metadata[0]++;
+            }, 0L, 5L);
+        }
+        else {
+            // Try every 5 Ticks if the item has landed
+            Bukkit.getScheduler().runTaskTimer(TesserTools.getPlugin(TesserTools.class), new Runnable() {
+                @Override
+                public void run() {
+                    if (metadata[0] >= 3) {
+                        Bukkit.getScheduler().cancelTasks(TesserTools.getPlugin(TesserTools.class));
+                    } else if (item.isOnGround()
+                            && item.isValid()
+                            && item.getLocation().getBlock().getState() instanceof EnchantingTable table
+                    ) {
+                        ExtEnchantingTable extTable = new ExtEnchantingTable(item.getLocation());
+                        if (extTable.isBlocked() || !extTable.isValid()) {
+                            return;
+                        }
+                        extTable.setBlocked(true);
+                        extTable.startEnchanting(item.getItemStack());
+
+                        Bukkit.getScheduler().cancelTasks(TesserTools.getPlugin(TesserTools.class));
+                    } else {
+                        metadata[0]++;
+                    }
                 }
-            }
-        }, 0L, 5L);
+            }, 0L, 5L);
+        }
+    }
+
+    /**
+     * Checks if the quarz block given is part of an enchanting table.
+     * @param loc The location of the quarz block
+     * @return An instance of an {@link ExtEnchantingTable} or null
+     */
+    private @Nullable ExtEnchantingTable getExtTable(Location loc) {
+        if (loc.clone().add(-4,0,-4).getBlock().getType() == Material.ENCHANTING_TABLE)
+        {
+            return new ExtEnchantingTable(loc.clone().add(-4,0,-4));
+        } else if (loc.clone().add(4,0,-4).getBlock().getType() == Material.ENCHANTING_TABLE) {
+            return new ExtEnchantingTable(loc.clone().add(4,0,-4));
+        } else if (loc.clone().add(4,0,4).getBlock().getType() == Material.ENCHANTING_TABLE) {
+            return new ExtEnchantingTable(loc.clone().add(4,0,4));
+        } else if (loc.clone().add(-4,0,4).getBlock().getType() == Material.ENCHANTING_TABLE) {
+            return new ExtEnchantingTable(loc.clone().add(-4,0,4));
+        } else {
+            return null;
+        }
     }
 }
